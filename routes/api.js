ad551f197153944c877cda2e8ba8c4770e737ad5
AngoJay <angojeff433@gmail.com>
2021-08-19 04:11:35 +0100

    database config

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const Laws = require("../model/laws");
-const Part_2 = require("../model/part_2");
-const Part_3 = require("../model/part_3");
 
 // create post
 router.post("/", (req, res) => {
