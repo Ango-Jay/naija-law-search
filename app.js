838884b68e42a9b9ab4f84123086c94cbdb30825
Ango-Jay <angojeff433@gmail.com>
2022-03-02 22:21:12 +0100

    wip

--- app.js
+++ app.js
@@ -21,6 +21,14 @@ const db = mongoose.connection;
 
 //Bind connection to error event (to get notification of connection errors)
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
+//Server static assets if in production
+if (process.env.NODE_ENV === "production") {
+  //Set static folder
+  app.use(express.static(path.join(__dirname, "client", "build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 // start server
 app.listen(port);
 console.log(`server is running on port ${port}`);
